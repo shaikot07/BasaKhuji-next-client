e5bab14a7fd04d6a1405ca5e1a8af1ece916cd5e
shaikot07 <saifulislamshaikat007@gmail.com>
2025-03-20 23:21:35 +0600

    make about us page route

---
+++ src/app/(WithCommonLayout)/aboutus/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const aboutUsPage = () => {
+    return (
+        <div>
+            <h2>this is about us page</h2>
+        </div>
+    );
+};
+
+export default aboutUsPage;
