26b366a8fabce3a300c4b9a8428708ba30e63a33
shaikot07 <saifulislamshaikat007@gmail.com>
2025-05-07 04:17:37 +0600

    add the credential for log in

--- src/components/modules/auth/login/LoginForm.tsx
+++ src/components/modules/auth/login/LoginForm.tsx
@@ -1,3 +1,122 @@
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// "use client";
+// import { Button } from "@/components/ui/button";
+// import {
+//   Form,
+//   FormControl,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { Input } from "@/components/ui/input";
+// import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+// import Link from "next/link";
+// import Logo from "@/assets/svgs/Logo";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { loginUser,  } from "@/services/AuthService";
+// import { toast } from "sonner";
+// import { loginSchema } from "./loginValidation";
+
+// import { useRouter, useSearchParams } from "next/navigation";
+// import { useUser } from "@/context/UserContext";
+
+// export default function LoginForm() {
+//   const form = useForm({
+//     resolver: zodResolver(loginSchema),
+//   });
+
+
+
+//   const searchParams = useSearchParams();
+//   const redirect = searchParams.get("redirectPath");
+//   const router = useRouter();
+//   const {setIsLoading}=useUser(); //ai ta add korren
+
+//   const {
+//     formState: { isSubmitting },
+//   } = form;
+
+
+
+//   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+//     try {
+//       const res = await loginUser(data);   //here is the login function
+//       setIsLoading(true)
+//       if (res?.success) {
+//         toast.success(res?.message);
+//         if (redirect) {
+//           router.push(redirect);
+//         } else {
+//           router.push("/");
+//         }
+//       } else {
+//         toast.error(res?.message);
+//       }
+//     } catch (err: any) {
+//       console.error(err);
+//     }
+//   };
+
+//   return (
+//     <div className="border-2 border-gray-300 rounded-xl flex-grow max-w-md w-full p-5">
+//       <div className="flex items-center space-x-4 ">
+//         <Logo />
+//         <div>
+//           <h1 className="text-xl font-semibold">Login</h1>
+//           <p className="font-extralight text-sm text-gray-600">Welcome back!</p>
+//         </div>
+//       </div>
+//       <Form {...form}>
+//         <form onSubmit={form.handleSubmit(onSubmit)}>
+//           <FormField
+//             control={form.control}
+//             name="email"
+//             render={({ field }) => (
+//               <FormItem>
+//                 <FormLabel>Email</FormLabel>
+//                 <FormControl>
+//                   <Input type="email" {...field} value={field.value || ""} />
+//                 </FormControl>
+//                 <FormMessage />
+//               </FormItem>
+//             )}
+//           />
+//           <FormField
+//             control={form.control}
+//             name="password"
+//             render={({ field }) => (
+//               <FormItem>
+//                 <FormLabel>Password</FormLabel>
+//                 <FormControl>
+//                   <Input type="password" {...field} value={field.value || ""} />
+//                 </FormControl>
+//                 <FormMessage />
+//               </FormItem>
+//             )}
+//           />
+
+//           <Button
+//             // disabled={isSubmitting}
+//             type="submit"
+//             className="mt-5 w-full"
+//           >
+//             {isSubmitting ? "Logging...." : "Login"}
+//           </Button>
+//         </form>
+//       </Form>
+//       <p className="text-sm text-gray-600 text-center my-3">
+//         Do not have any account? &nbsp; 
+//         <Link href="/register" className="text-primary">
+//         <span className="text-blue-600 underline">Register</span> 
+//         </Link>
+//       </p>
+//     </div>
+//   );
+// }
+
+
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { Button } from "@/components/ui/button";
@@ -14,10 +133,9 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import Link from "next/link";
 import Logo from "@/assets/svgs/Logo";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { loginUser,  } from "@/services/AuthService";
+import { loginUser } from "@/services/AuthService";
 import { toast } from "sonner";
 import { loginSchema } from "./loginValidation";
-
 import { useRouter, useSearchParams } from "next/navigation";
 import { useUser } from "@/context/UserContext";
 
@@ -26,23 +144,19 @@ export default function LoginForm() {
     resolver: zodResolver(loginSchema),
   });
 
-
-
   const searchParams = useSearchParams();
   const redirect = searchParams.get("redirectPath");
   const router = useRouter();
-  const {setIsLoading}=useUser(); //ai ta add korren
+  const { setIsLoading } = useUser();
 
   const {
     formState: { isSubmitting },
   } = form;
 
-
-
   const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     try {
-      const res = await loginUser(data);   //here is the login function
-      setIsLoading(true)
+      const res = await loginUser(data);
+      setIsLoading(true);
       if (res?.success) {
         toast.success(res?.message);
         if (redirect) {
@@ -60,13 +174,36 @@ export default function LoginForm() {
 
   return (
     <div className="border-2 border-gray-300 rounded-xl flex-grow max-w-md w-full p-5">
-      <div className="flex items-center space-x-4 ">
+      <div className="flex items-center space-x-4">
         <Logo />
         <div>
           <h1 className="text-xl font-semibold">Login</h1>
           <p className="font-extralight text-sm text-gray-600">Welcome back!</p>
         </div>
       </div>
+
+      {/* Just Set Credentials */}
+      <div className="flex justify-center gap-4 mt-4">
+        <Button
+          variant="outline"
+          onClick={() => {
+            form.setValue("email", "test1@gmail.com");
+            form.setValue("password", "123456");
+          }}
+        >
+          Landlord
+        </Button>
+        <Button
+          variant="outline"
+          onClick={() => {
+            form.setValue("email", "admin@gmail.com");
+            form.setValue("password", "123456");
+          }}
+        >
+          Admin
+        </Button>
+      </div>
+
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)}>
           <FormField
@@ -96,23 +233,18 @@ export default function LoginForm() {
             )}
           />
 
-          <Button
-            // disabled={isSubmitting}
-            type="submit"
-            className="mt-5 w-full"
-          >
-            {isSubmitting ? "Logging...." : "Login"}
+          <Button type="submit" className="mt-5 w-full">
+            {isSubmitting ? "Logging in..." : "Login"}
           </Button>
         </form>
       </Form>
+
       <p className="text-sm text-gray-600 text-center my-3">
-        Do not have any account? &nbsp; 
+        Do not have any account? &nbsp;
         <Link href="/register" className="text-primary">
-        <span className="text-blue-600 underline">Register</span> 
+          <span className="text-blue-600 underline">Register</span>
         </Link>
       </p>
     </div>
   );
 }
-
-
